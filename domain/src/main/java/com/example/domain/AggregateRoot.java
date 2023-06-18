4b13b784fa7f51aaebdb70e6a9a676afd7d22f30
André Pereira <andrepereira97@yahoo.com>
2023-06-18 16:16:13 -0300

    :sparkles: make AggregateRoot abstract

--- domain/src/main/java/com/example/domain/AggregateRoot.java
+++ domain/src/main/java/com/example/domain/AggregateRoot.java
@@ -1,6 +1,6 @@
 package com.example.domain;
 
-public class AggregateRoot<ID extends Identifier> extends Entity<ID> {
+public abstract class AggregateRoot<ID extends Identifier> extends Entity<ID> {
   protected AggregateRoot(ID id) {
     super(id);
   }
